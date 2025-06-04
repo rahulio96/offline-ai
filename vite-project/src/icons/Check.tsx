b92e6782abd0093a782ec35c4a929c4be6736ec6
rahulio96 <rahgupta96@gmail.com>
2025-06-03 17:46:43 -0700

    Fix svg icon attributes to remove console errors

--- vite-project/src/icons/Check.tsx
+++ vite-project/src/icons/Check.tsx
@@ -4,6 +4,6 @@ interface props {
 
 export default function Check({color}: props) {
   return (
-    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke={color}  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" /></svg>
+    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke={color}  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" /></svg>
   )
 }
